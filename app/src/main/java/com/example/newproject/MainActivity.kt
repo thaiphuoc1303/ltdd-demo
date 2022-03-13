60104837c3be3372ea845bddaf0e21b61b788f40
phuoc <thaiphuoc1303@gmail.com>
2022-03-14 01:39:30 +0700

    add comment

--- app/src/main/java/com/example/newproject/MainActivity.kt
+++ app/src/main/java/com/example/newproject/MainActivity.kt
@@ -22,19 +22,24 @@ class MainActivity : AppCompatActivity() {
         val btnGetTime : Button = findViewById(R.id.btnGetTime)
         val tvTime : TextView = findViewById(R.id.tvTime)
         val list : ListView = findViewById(R.id.list)
-
+        //tao mang du lieu
         val strList = ArrayList<String>()
+        //tao adapter
         val adapterView = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, strList)
-
+        //gan adapter cho listview
         list.adapter =adapterView
+        //tao Datetime format
         val formatter = DateTimeFormatter.ofPattern("hh:mm:ss a")
+        //Set su kien cho button
         btnGetTime.setOnClickListener{
+            //lay gio hien tai
             val current = LocalDateTime.now()
             val formatted = current.format(formatter)
+            //gan du lieu cho TextView
             tvTime.text=formatted
-
+            //them du lieu vao mang
             strList.add(formatted)
-            strList.removeAt(0)
+            //cap nhat adapter
             adapterView.notifyDataSetChanged()
         }
     }
